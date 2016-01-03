c72d9389cbaf00e4e070893494014cea1993a46c
PoppyLi <83613578@qq.com>
2016-01-03 22:27:04 +0800

    upload

--- web/views/admin/common/footer.php
+++ web/views/admin/common/footer.php
@@ -1,11 +1,8 @@
-    <!-- jQuery -->
-    <script src="/static/bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="/static/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="/static/js/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/static/js/bootstrap.min.js"></script>
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="/static/bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
+    <script src="/static/js/metisMenu.min.js"></script>
     <!-- Custom Theme JavaScript -->
-    <script src="/static/dist/js/sb-admin-2.js"></script>
+    <script src="/static/js/sb-admin-2.js"></script>
